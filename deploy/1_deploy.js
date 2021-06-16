11ea349ba3eb8a5503fa57537d65786316487391
bbenligiray <bbenligiray@gmail.com>
2021-06-16 14:30:33 +0300

    Update deployment

--- deploy/1_deploy.js
+++ deploy/1_deploy.js
@@ -3,7 +3,7 @@ module.exports = async ({ getUnnamedAccounts, deployments }) => {
   const accounts = await getUnnamedAccounts();
 
   const erc20Faucet = await deploy('ERC20Faucet', {
-    args: ["0xb015ACeEdD478fc497A798Ab45fcED8BdEd08924", "1000000000000000000000"],
+    args: ["0xc8fed58d49c9fd3d2690e0eb29cde9678cc45a9a", "1000000000000000000000"],
     from: accounts[0],
   });
   log(`Deployed ERC20Faucet at ${erc20Faucet.address}`);
